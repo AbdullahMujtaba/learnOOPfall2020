419563178e5914be7e2efc2208f44a378b959e7f
FLH <mainabdullah001@gmail.com>
2020-09-02 19:50:07 +0500

    This is my lab 3

---
+++ Lab/Lab 3.py
@@ -0,0 +1,10 @@
+# Creating the Dictionaries
+stdDict_1 = {"name": "AbdulKabeerAbid", "age": 19, "rollno": "BDSM-S20-005"}
+stdDict_2 = {"name": "Abdullah Mujtaba", "age": 19, "rollno": "BDSM-S20-003"}
+stdDict_3 = {"name": "Hassan Razzaq", "age": 20, "rollno": "BDSM-S20-001"}
+@@ -25,4 +25,4 @@
+print(listStudent_Record[1]["name"])
+print(listStudent_Record[2]["name"])
+print(listStudent_Record[3]["name"])
+print(listStudent_Record[4]["name"])
+print(listStudent_Record[4]["name"])
