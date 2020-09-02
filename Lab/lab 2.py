8b218dfd0ab980e73487fb3cf2a2ec4e02ce302b
FLH <mainabdullah001@gmail.com>
2020-09-02 19:33:48 +0500

    This is Lab 2

---
+++ Lab/lab 2.py
@@ -0,0 +1,29 @@
+student1 = {"name": "AbulKabeer", "age": 19, "marks": 40}
+print(student1)                     # printing a dictionary
+
+#printing values of the dictionary
+
+print("Name of student:")
+print(student1["name"])
+
+print("Age of student:")
+print(student1["age"])
+
+print("Marks of student:")
+print(student1["marks"])
+
+#Creating another dictionary
+#            key   :  ans            key  : ans   key   : ans
+
+student2 = {"name" : "qasim ilyas", "age" : 19, "marks" : 55}
+
+#printing answers from the dictionary (student1)
+
+print("\n")
+print(student2)
+print("Name of Student: ")
+print(student2["name"])
+print("Age of Student: ")
+print(student2["age"])
+print("Marks of Student: ")
+print(student2["marks"])
