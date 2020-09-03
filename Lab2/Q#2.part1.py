8a00648919b187eabff1deb90d20ab19d9effe96
abdulkabeer121 <kabeer.abid419@gmail.com>
2020-09-03 11:35:55 -0700

    Here is Q # 2 OOP Task

---
+++ Lab2/Q#2.part1.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number to Check weather it is even or odd: "))
+
+if number % 2 == 0:
+
+    print ("The Number You Entered Is Even")
+
+else:
+    print ("The Number You Entered Is Odd")
